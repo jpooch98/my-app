e6857f9f447f138ce7180cb605cf69ffbb3ec13b
James <45608681+jpooch98@users.noreply.github.com>
2023-01-27 14:21:49 -0500

    work

--- src/index.js
+++ src/index.js
@@ -1,31 +1,72 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import './index.css';
+
+const books = [
+    {
+        image: "https://images-na.ssl-images-amazon.com/images/I/91Szm8FpdfL._AC_UL900_SR900,600_.jpg",
+        title: "Spare",
+        author: "Prince Harry",
+        id: 1,
+    },
+    {
+        image: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+        title: "Atomic Habbits",
+        author: "James Cleary",
+        id: 2,
+    }
+];
+
 function BookList() {
     return (
-        <section>
-            <Book />
-            <Book />
-            <Book />
-        </section>
+        <div>
+            <h1 className="center">Amazon's Best Sellers list</h1>
+            <section className="BookList">
+                <EventExample />
+                {books.map((book) => {
+                    const { image, title, author, id } = book
+                    return (
+                        <Book {...book} key={id} />
+                    )
+                })}
+            </section>
+        </div>
     );
 };
 
-const Book = () => {
+const EventExample = () => {
+    const handleFormInput = (e) => {
+        console.log(e.target)
+        console.log(e.target.name)
+        console.log(e.target.value)
+        console.log('Handle Form input')
+    }
+    const handleButtonClick = () => {
+        alert('Button pressed')
+    }
+    const handleFormSubmission = (e) => {
+        e.preventDefault();
+        console.log('form Sumbitted')
+    }
+    return <section>
+        <form onSubmit={handleFormSubmission}>
+            <h2>Typical form</h2>
+            <input type="text" name="example" onChange={handleFormInput} style={{ margin: '1rems 0' }}></input>
+        </form>
+        <button onClick={handleButtonClick}>Click Me</button>
+    </section>
+}
+
+const Book = (props) => {
     return (
-        <article>
-            <Image />
-            <Title />
-            <Author />
+        <article className="Book">
+            <img src={props.image} />
+            <h1><b>{props.title}</b></h1>
+            <p>{props.author}</p>
         </article>
     );
 };
-
-const Image = () => <img src="https://m.media-amazon.com/images/I/41hYQIILU3L.jpg" />;
-
-const Title = () => <h1><b>Spare</b></h1>;
-
-const Author = () => <p>Prince Harry</p>;
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
